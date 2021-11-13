374cb348e4cc02766e693d989369fd72e80308b4
Shopnil <shopnilmax@gmail.com>
2021-11-13 15:01:49 +0600

    t4_1 renamed to L_Converter

---
+++ Re.0/L_Converter.py
@@ -0,0 +1,17 @@
+def converter(phrase):
+    conversion=""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                conversion=conversion+"B"
+            else:
+                conversion=conversion+"b"
+        
+        else:
+            conversion=conversion+letter
+    return conversion
+
+print(converter(input("Enter your text: \n")))
+
+while True:
+    print(converter(input("Enter your text: \n")))
