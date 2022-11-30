4785cfced6f464b851963688b94d2f3a364767ce
Shopnil <shopnilmax@gmail.com>
2022-11-30 20:56:30 +0600

    Image Asset Added

--- Re.0/number_in_words.py
+++ Re.0/number_in_words.py
@@ -1,8 +1,21 @@
 import os
 from gtts import gTTS
 from tkinter import *
+from PIL import Image, ImageTk
 func_root = Tk()
 # gui logic here
+func_root.geometry("1280x720")
+func_root.minsize(480, 360)
+func_root.maxsize(1920, 1080)
+title = Label(text="Number in Words Beta")
+title.pack()
+j_image = Image.open('asset.jpg')
+Out_j_image = ImageTk.PhotoImage(j_image)
+imagery_b = Label(image=Out_j_image)
+imagery_b.pack()
+#b_image = PhotoImage(file="1.png")
+#imagery_b = Label(image=b_image)
+# imagery_b.pack()
 func_root.mainloop()
 language = 'en'
 numbers_dict = {"0": "zero", "1": "one", "2": "two", "3": "three", "4": "five", "5": "five", "6": "six",
